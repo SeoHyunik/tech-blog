ace050c12770f0899a28023d0074941a879a80f5
SeoHyunik <hos0917@naver.com>
2024-11-26 13:46:22 +0900

    .

--- src/main/java/com/automatic/tech_blog/utils/WordPressUtils.java
+++ src/main/java/com/automatic/tech_blog/utils/WordPressUtils.java
@@ -8,7 +8,6 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
