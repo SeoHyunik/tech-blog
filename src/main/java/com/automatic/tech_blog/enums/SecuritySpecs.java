446a060c72d9f4fb8d88d313860ad6724ede7fa0
SeoHyunik <hos0917@naver.com>
2024-12-10 16:44:56 +0900

    .

--- src/main/java/com/automatic/tech_blog/enums/SecuritySpecs.java
+++ src/main/java/com/automatic/tech_blog/enums/SecuritySpecs.java
@@ -5,7 +5,7 @@ public enum SecuritySpecs {
   SECRET_KEY_ALGORITHM("AES"),
   ENV_DIR_PATH("env"),
   ENV_KEY_NAME("SECRET_KEY_BASE64"),
-  CREDENTIAL_FILE_PATH("src/main/resources/encrypted/encrypted_credentials.txt"),
+  CREDENTIAL_FILE_PATH("src/main/resources/encrypted/encrypted_credential.txt"),
   OPEN_AI_SECRET_KEY_FILE_PATH("src/main/resources/encrypted/openai_secret_key.txt"),
   KIWIJAM_PW_FILE_PATH("src/main/resources/encrypted/kiwijam_pw.txt"),
   EXCHANGE_RATE_API_KEY_FILE_PATH("src/main/resources/encrypted/exchange_rate_api_key.txt"),
